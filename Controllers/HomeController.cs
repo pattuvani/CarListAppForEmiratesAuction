62440ad8d52cdbea09759e853d1de3b28e238598
pattuvani <pattu_vani@yahoo.co.in>
2020-10-26 13:12:26 +0400

    Updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using CarListAppForEmiratesAuction.Models;
 
 namespace CarListAppForEmiratesAuction.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        
+            /// <summary>
+            /// To Get Car List
+            /// </summary>
+            /// <returns></returns>
+            public async Task<ActionResult> Index()
+            {
+
+                try
+                {
+                    string apiUrl = "https://api.eas.ae/v2/carsonline?source=mweb";
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(apiUrl);
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var data = await response.Content.ReadAsStringAsync();
+                            JavaScriptSerializer js = new JavaScriptSerializer();
+                            CarViewModel.Root CarObject = js.Deserialize<CarViewModel.Root>(data);
+                            //To Get load more data
+                            if (Request.HttpMethod == "POST")
+                            {
+                                ViewBag.jsondata = CarObject.Cars.Skip(10).Take(20);
+                            }
+                            else
+                                ViewBag.jsondata = CarObject.Cars.Take(10);
+                        }
+                        else
+                        {
+                            ViewBag.jsondata = "Internal server Error,please contact administrator";
+                        }
+                    }
+                    return View(ViewBag.jsondata);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            /// <summary>
+            /// To Get Refresh Car List
+            /// </summary>
+            /// <returns></returns>
+            public async Task<ActionResult> RefreshCarList()
+            {
+                try
+                {
+                    string apiUrl = "https://api.eas.ae/v2/carsonline?source=mweb";
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(apiUrl);
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var data = await response.Content.ReadAsStringAsync();
+                            JavaScriptSerializer js = new JavaScriptSerializer();
+                            CarViewModel.Root CarObject = js.Deserialize<CarViewModel.Root>(data);
+                            ViewBag.jsondata = CarObject.Cars.Take(10);
+                        }
+                        else
+                        {
+                            ViewBag.jsondata = "Internal server Error,please contact administrator";
+                        }
+                    }
+                    return RedirectToAction("Index", "Home", ViewBag.jsondata);
+
+                }
+                catch (Exception ex)
+                {
+                    //return View("somthing went wrong");
+                    throw ex;
+
+                }
+            }
+
+            /// <summary>
+            /// To Get Filter car list by make
+            /// </summary>
+            /// <param name="FilteredList"></param>
+            /// <returns></returns>
+            public ActionResult FilterList(List<CarViewModel> FilteredList)
+            {
+                ViewBag.Filtereddata = TempData["FilterData"];
+                return View(ViewBag.Filtereddata);
+            }
+
+            /// <summary>
+            /// To Get Filter car list by make
+            /// </summary>
+            /// <param name="searchtxt"></param>
+            /// <returns></returns>
+            [HttpPost]
+            public async Task<ActionResult> FilterList(string searchtxt)
+            {
+                try
+                {
+                    searchtxt = searchtxt.ToUpper();
+                    string apiUrl = "https://api.eas.ae/v2/carsonline?source=mweb?";
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri(apiUrl);
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var data = await response.Content.ReadAsStringAsync();
+                            JavaScriptSerializer js = new JavaScriptSerializer();
+                            CarViewModel.Root CarObject = js.Deserialize<CarViewModel.Root>(data);
+                            TempData["FilterData"] = CarObject.Cars.Where(c => c.makeEn.ToUpper().Contains(searchtxt));
+                        }
+                        else
+                        {
+                            ViewBag.jsondata = "Internal server Error,please contact administrator";
+                        }
+                    }
+
+                    return RedirectToAction("FilterList", "Home", TempData["FilterData"]);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            
+
+            }
+
+        /// <summary>
+        /// To Get car list by Ajax
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult Ajaxview()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ Models/CarViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarListAppForEmiratesAuction.Models
+{
+    public class CarViewModel
+    {
+        public class Error
+        {
+        }
+
+        public class UaeTime
+        {
+            public string en { get; set; }
+            public string ar { get; set; }
+        }
+
+        public class AuctionInfo
+        {
+            public int bids { get; set; }
+            public int endDate { get; set; }
+            public string endDateEn { get; set; }
+            public string endDateAr { get; set; }
+            public string currencyEn { get; set; }
+            public string currencyAr { get; set; }
+            public int currentPrice { get; set; }
+            public int minIncrement { get; set; }
+            public int lot { get; set; }
+            public int priority { get; set; }
+            public int VATPercent { get; set; }
+            public int itemAuctionType { get; set; }
+            public int isModified { get; set; }
+            public int itemid { get; set; }
+            public int iCarId { get; set; }
+            public string iVinNumber { get; set; }
+        }
+
+        public class Car
+        {
+            public int carID { get; set; }
+            public string image { get; set; }
+            public int year { get; set; }
+            public string makeEn { get; set; }
+            public string makeAr { get; set; }
+            public string modelEn { get; set; }
+            public string modelAr { get; set; }
+            public string bodyEn { get; set; }
+            public string bodyAr { get; set; }
+            public AuctionInfo AuctionInfo { get; set; }
+        }
+
+        public class Root
+        {
+            public string alertEn { get; set; }
+            public string alertAr { get; set; }
+            public Error Error { get; set; }
+            public int RefreshInterval { get; set; }
+            public string Ticks { get; set; }
+            public int count { get; set; }
+            public int endDate { get; set; }
+            public string sortOption { get; set; }
+            public string sortDirection { get; set; }
+            public UaeTime uaeTime { get; set; }
+            public int updateList { get; set; }
+            public int minYear { get; set; }
+            public int maxYear { get; set; }
+            public int minPrice { get; set; }
+            public int maxPrice { get; set; }
+            public int page { get; set; }
+            public int totalPage { get; set; }
+            public int itemPerPage { get; set; }
+            public List<Car> Cars { get; set; }
+        }
+
+
+    }
+}
